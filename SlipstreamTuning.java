fa41af89d96ecca5b85b0b00cdcdaa84f3bc947a
helotyr <psahaj875@gmail.com>
2026-07-21 23:34:36 -0400

    Update SlipstreamTuning.java

--- SlipstreamTuning.java
+++ SlipstreamTuning.java
@@ -4,6 +4,7 @@ import static org.firstinspires.ftc.teamcode.SlipstreamTuning.panel;
 import static org.firstinspires.ftc.teamcode.SlipstreamTuning.setPowers;
 import static org.firstinspires.ftc.teamcode.SlipstreamTuning.stopMotors;
 import static org.firstinspires.ftc.teamcode.SlipstreamTuning.motors;
+import static org.firstinspires.ftc.teamcode.SlipstreamTuning.constants;
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.configurables.annotations.IgnoreConfigurable;
 import com.bylazar.telemetry.PanelsTelemetry;
